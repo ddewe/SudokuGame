cb62ada450c8c78b03ab947859415958f610d259
ddewe <ddewee92@gmail.com>
2021-02-24 19:20:46 +0100

    Created coordinates class

--- src/sudoku/problemdomain/SudokuGame.java
+++ src/sudoku/problemdomain/SudokuGame.java
@@ -1,4 +1,23 @@
 package sudoku.problemdomain;
 
-public class SudokuGame {
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyOfGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
+
+    private final GameState gameState;
+    private final int [][] gridState;
+
+    public static final int RID_BOUNDARY = 9;
 }
